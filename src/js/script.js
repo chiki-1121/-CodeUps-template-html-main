b35b8172ecc60ef9c20b8569f15a1336b20caa42
chiki-1121 <tr.giraffe@gmail.com>
2023-07-29 10:46:45 +0900

    js実装

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,35 @@
-
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+    let topBtn = $('.c-to-top');
+    topBtn.hide();
+
+    // ボタンの表示設定
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 70) {
+            // 指定px以上のスクロールでボタンを表示
+            topBtn.fadeIn();
+        } else {
+            // 画面が指定pxより上ならボタンを非表示
+            topBtn.fadeOut();
+        }
+    });
+
+    // ヘッダークラス名付与
+
+    let header = $('.p-header');
+    //ヘッダーの高さを取得
+    let headerHeight = $('.p-header').height();
+    //メインビューの高さを取得
+    let height = $('.p-mv').height();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > (height - headerHeight)) {
+            header.addClass('is-color');
+        } else {
+            header.removeClass('is-color');
+        }
+    });
+
     //ドロワーメニュー
     $(".js-hamburger").click(function () {
         if ($(".js-hamburger").hasClass('is-active')) {
