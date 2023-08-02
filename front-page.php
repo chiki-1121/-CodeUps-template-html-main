ba0e3152d648e2bad59e56dd820b4fbdcd739229
chiki-1121 <tr.giraffe@gmail.com>
2023-08-02 10:49:08 +0900

    ライブコーディングの通りの実装完了！

--- front-page.php
+++ front-page.php
@@ -1,5 +1,15 @@
 <?php get_header(); ?>
 
+<?php
+$home = esc_url(home_url('/'));
+$news = esc_url(home_url('/news/'));
+$service = esc_url(home_url('/service/'));
+$works = esc_url(home_url('/works/'));
+$company = esc_url(home_url('/company/'));
+$recruit = esc_url(home_url('/recruit/'));
+$contact = esc_url(home_url('/contact/'));
+?>
+
 <section class="l-mv p-mv js-mv-height">
   <div class="p-mv__inner">
     <div class="p-mv__header">
@@ -10,7 +20,7 @@
         お客様の夢を叶える<br class="u-mobile">Webサイトを制作
       </p>
       <div class="p-mv__btn">
-        <a href="#" class="c-mv-btn">CONTACT</a>
+        <a href="<?php echo $contact; ?>" class="c-mv-btn">CONTACT</a>
       </div>
     </div>
     <div class="p-mv__scroll"><span>SCROLL</span></div>
@@ -24,35 +34,29 @@
       <p class="c-section-header__jatitle">お知らせ</p>
     </div>
     <div class="p-news__items">
-      <!-- ループ処理 -->
-      <div class="p-news__item p-news-content">
-        <div class="p-news-content__meta">
-          <time datetime="2023-07-23" class="p-news-content__date">2023.7.23</time>
-          <p class="p-news-content__category">お知らせ</p>
-        </div>
-        <div class="p-news-content__link">
-          <a href="#" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
-        </div>
-      </div>
-      <!-- ループ終了 -->
-      <div class="p-news__item p-news-content">
-        <div class="p-news-content__meta">
-          <time datetime="2023-07-23" class="p-news-content__date">2023.7.23</time>
-          <p class="p-news-content__category">お知らせ</p>
-        </div>
-        <div class="p-news-content__link">
-          <a href="#" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
-        </div>
-      </div>
-      <div class="p-news__item p-news-content">
-        <div class="p-news-content__meta">
-          <time datetime="2023-07-23" class="p-news-content__date">2023.7.23</time>
-          <p class="p-news-content__category">お知らせ</p>
-        </div>
-        <div class="p-news-content__link">
-          <a href="#" class="p-news-content__text">Webデザインニュースサイト「ウェブマガジン」に取材いただきました</a>
-        </div>
-      </div>
+      <?php
+      $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => 3,
+      );
+      $post_query = new WP_Query($args);
+      if ($post_query->have_posts()) : while ($post_query->have_posts()) : $post_query->the_post(); ?>
+          <div class="p-news__item p-news-content">
+            <div class="p-news-content__meta">
+              <time datetime="<?php the_time('c'); ?>3" class="p-news-content__date"><?php the_time('Y.m.d'); ?></time>
+              <p class="p-news-content__category">
+                <?php $category = get_the_category();
+                echo '<span>' . $category[0]->name . '</span>'; ?>
+              </p>
+            </div>
+            <div class="p-news-content__link">
+              <a href="<?php the_permalink(); ?>" class="p-news-content__text"><?php the_title(); ?></a>
+            </div>
+          </div>
+      <?php endwhile;
+      endif;
+      wp_reset_postdata(); ?>
+
     </div>
   </div>
 </section>
@@ -93,7 +97,7 @@
       </div>
     </div>
     <div class="p-service__btn">
-      <a href="" class="c-button">more</a>
+      <a href="<?php echo $service; ?>" class="c-button">more</a>
     </div>
   </div>
 </section>
@@ -105,15 +109,23 @@
         <!-- Swiper -->
         <div class="swiper js-works-swiper">
           <div class="swiper-wrapper">
-            <div class="swiper-slide p-works__img">
-              <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/works_1.jpg" alt="">
-            </div>
-            <div class="swiper-slide p-works__img">
-              <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/service_1.jpg" alt="">
-            </div>
-            <div class="swiper-slide p-works__img">
-              <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/common/service_2.jpg" alt="">
-            </div>
+            <?php
+            $args = array(
+              'post_type' => 'works',
+              'posts_per_page' => -1,
+            );
+            $works_query = new WP_Query($args);
+            if ($works_query->have_posts()) : while ($works_query->have_posts()) : $works_query->the_post(); ?>
+                <div class="swiper-slide p-works__img">
+                  <?php if (has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail(); ?><!-- アイキャッチ画像指定されている場合 -->
+                  <?php else : ?>
+                    <img src='<?php echo get_template_directory_uri() ?>/dist/assets/images/common/noimg.png'><!-- アイキャッチ画像指定されていない場合に代替画像を表示 -->
+                  <?php endif; ?>
+                </div>
+            <?php endwhile;
+            endif;
+            wp_reset_postdata(); ?>
           </div>
         </div>
         <div class="swiper-pagination js-works-pagination"></div>
@@ -130,7 +142,7 @@
           </p>
         </div>
         <div class="p-works__btn">
-          <a href="" class="c-button">more</a>
+          <a href="<?php echo $works; ?>" class="c-button">more</a>
         </div>
       </div>
     </div>
@@ -158,7 +170,7 @@
         </p>
       </div>
       <div class="p-company__btn">
-        <a href="" class="c-button">more</a>
+        <a href="<?php echo $company; ?>" class="c-button">more</a>
       </div>
     </div>
   </div>
@@ -181,7 +193,7 @@
           </p>
         </div>
         <div class="p-recruit__btn">
-          <a href="" class="c-button">more</a>
+          <a href="<?php echo $recruit; ?>" class="c-button">more</a>
         </div>
       </div>
     </div>
